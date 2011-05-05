8b7a1e19e76bce0dd068dc60b9f8ebf39da4bb67
David Van Horn <dvanhorn@ccs.neu.edu>
2011-05-04 22:22:53 -0400

    Updated release notes.

--- planet/info.rkt
+++ planet/info.rkt
@@ -2,12 +2,12 @@
 (define name "Backgammon.")
 (define scribblings '(("scribblings/backgammon.scrbl")))
 (define categories '(misc))
-(define required-core-version "5.0.1")
+(define required-core-version "5.1.1")
 (define repositories (list "4.x"))
 (define primary-file 
-  '("main.ss"))
+  '("main.rkt"))
 (define blurb
   (list '(div "Backgammon game and server.")))
 (define release-notes 
   (list
-   '(div "Updated for 5.0.1.")))
+   '(div "Updated for 5.1.1.")))
